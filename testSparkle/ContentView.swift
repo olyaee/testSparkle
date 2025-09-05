e34d56f602c9cd5c42357ab6621cb467e875be35
olyaee <olyaee.ehsan@gmail.com>
2025-09-05 18:07:38 +0200

    Initial Commit

---
+++ testSparkle/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  testSparkle
+//
+//  Created by ehsan olyaee on 05.09.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ testSparkle/testSparkleApp.swift
@@ -0,0 +1,17 @@
+//
+//  testSparkleApp.swift
+//  testSparkle
+//
+//  Created by ehsan olyaee on 05.09.25.
+//
+
+import SwiftUI
+
+@main
+struct testSparkleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
